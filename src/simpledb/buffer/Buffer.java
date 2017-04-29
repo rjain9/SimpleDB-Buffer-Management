4854ba754c4faf57f9fed1e71d314d1b734a832c
skavuru <skavuru@ncsu.edu>
2017-04-29 19:52:06 -0400

    Modified the chooseUnpinned method

--- src/simpledb/buffer/Buffer.java
+++ src/simpledb/buffer/Buffer.java
@@ -187,4 +187,10 @@ public class Buffer {
       blk = contents.append(filename);
       pins = 0;
    }
+   
+   /*Returning the logSequenceNumber
+    * */
+   public int getLogSequenceNumber() {
+	      return logSequenceNumber;
+	   }
 }
